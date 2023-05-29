02ee000c813370cf9c0eab9627570b5b5f432986
Nadezhda <79102420+not-fate@users.noreply.github.com>
2023-05-29 16:55:21 +0300

    Delete Лаб.2. Эхо-сервер directory

--- Лаб.2. Эхо-сервер/test.py
+++
@@ -1,7 +0,0 @@
-import _md5
-
-salt = 'i wanna die'
-
-password = '1234'
-password2 = '1234'
-print(_md5.md5((password + salt).encode()).hexdigest() == _md5.md5((password2 + salt).encode()).hexdigest())
